aca3cd6d8146609defee66372fdf27c85da41b1e
BootCoder <bootcoder@gmail.com>
2017-01-11 00:18:27 -0800

    switch feed to dbc-infowall-api

--- app/assets/javascripts/cardLoader.js.jsx
+++ app/assets/javascripts/cardLoader.js.jsx
@@ -6,41 +6,89 @@ var renderReactPeople = function (data) {
   React.render(<PersonCardContainer people={data}/>, document.getElementById('content'))
 }
 
+// var getEvents = function () {
+//   $.get('/events/cards.json', function( data ) {
+//     if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+//     renderReactEvents(data)
+//   })
+// }
+
+// var getMentors = function () {
+//   $.get('/mentors.json', function (data) {
+//     if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+//     renderReactPeople(data)
+//   })
+// }
+
+// var getStaff = function () {
+//   $.get('/staff.json', function (data) {
+//     if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+//     renderReactPeople(data)
+//   })
+// }
+
+// var getToday = function () {
+//   $.get('/cards.json', function (data) {
+    // console.log(data)
+//     if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+//     renderReactEvents(data)
+//   })
+// }
+
+// var getCalendar = function () {
+//   $.get( '/calendars.json', function (data) {
+//     renderReactEvents(data)
+//   })
+// }
+
+
 var getEvents = function () {
-  $.get('/events/cards.json', function( data ) {
-    if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+  // console.log('hit getEvents')
+  $.get('https://dbc-infowall-api.herokuapp.com/events', function( data ) {
     renderReactEvents(data)
   })
 }
 
 var getMentors = function () {
-  $.get('/mentors.json', function (data) {
-    if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+  // console.log('hit getMentors')
+  $.get('https://dbc-infowall-api.herokuapp.com/mentors', function (data) {
+    if (data === null || data === undefined || data.length === 0) { return getEvents() }
     renderReactPeople(data)
   })
 }
 
 var getStaff = function () {
-  $.get('/staff.json', function (data) {
-    if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+  // console.log('hit getStaff')
+  $.get('https://dbc-infowall-api.herokuapp.com/staff', function (data) {
+    if (data === null || data === undefined || data.length === 0) { return getEvents() }
     renderReactPeople(data)
   })
 }
 
 var getToday = function () {
-  $.get('/cards.json', function (data) {
-    console.log(data)
-    if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+  // console.log('hit getToday')
+  $.get('https://dbc-infowall-api.herokuapp.com/today', function (data) {
+    // console.log(data)
+    if (data === null || data === undefined || data.length === 0) { return getEvents() }
+    renderReactEvents(data)
+  })
+}
+
+var getWorkshops = function () {
+  // console.log('hit getWorkshops')
+  $.get('https://dbc-infowall-api.herokuapp.com/workshops', function (data) {
+    // console.log(data)
+    if (data === null || data === undefined || data.length === 0) { return getEvents() }
     renderReactEvents(data)
   })
 }
 
 var getCalendar = function () {
+  // console.log('hit getCalendar')
   $.get( '/calendars.json', function (data) {
     renderReactEvents(data)
   })
 }
-
 var cycleCardView = function (counter) {
   console.log(counter)
   if (counter === 0) {
@@ -57,18 +105,22 @@ var cycleCardView = function (counter) {
     getCardsIf(counter)
   } else if (counter === 3) {
     counter = 0
-    getCalendar()
+    getWorkshops()
     getCardsIf(counter)
   }
 }
 
 var getCardsIf = function (counter) {
-  if (window.location.pathname === '/events/cards') {
+  if (window.location.pathname === '/events') {
     getEvents()
   } else if (window.location.pathname === '/mentors') {
     getMentors()
   } else if (window.location.pathname === '/staff') {
     getStaff()
+  } else if (window.location.pathname === '/workshops') {
+    getWorkshops()
+  } else if (window.location.pathname === '/today') {
+    getToday()
   } else if (window.location.pathname === '/cards' || window.location.pathname === '/') {
     setTimeout(function () { cycleCardView(counter) }, 40000)
   }
--- app/assets/javascripts/components/events/event_card_image.js.jsx
+++ app/assets/javascripts/components/events/event_card_image.js.jsx
@@ -1,9 +1,14 @@
 var EventCardImage = React.createClass({
 
   render: function() {
+    if(this.props.event.card_type == 'workshop'){
+      img_tag = <img src={this.props.event.img_url} className ="circle-60px right fullWidth" />
+    }else {
+      img_tag = <Img src={this.props.event.img_url} className ="circle-60px right fullWidth" />
+    }
     return (
       <div className="small-3 large-3 columns no-pad-left">
-        <Img src={this.props.event.img_url} className ="circle-60px right fullWidth" />
+        {img_tag}
       </div>
     );
   }
